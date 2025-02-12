0a5280ce847aab374bd66c17dc9c819bd2c98015
Stan <nolastan@gmail.com>
2025-02-12 07:50:50 +0000

    style date and url

--- src/App.js
+++ src/App.js
@@ -32,19 +32,32 @@ export default function App() {
         shops.forEach(shop => {
           const { name, address, start, description, lat, lng, image, website } = shop;
           const image_tag = image ? `<img src="${image}" alt="" style="width: 100%; aspect-ratio: 2; object-fit: cover; border-radius: 3px;" />` : '';
-          const date = new Date(start).toLocaleDateString('en-US', { weekday: 'long' })
+          const urlRegex = /(https?:\/\/[^\s]+)/;
+          const urlMatch = description.match(urlRegex);
+          const url = urlMatch ? urlMatch[0] : '';
+          const updatedDescription = description.replace(url, '');
+
+          const date = new Date(start);
+          const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'long' });
+          const fullDate = date.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+
           new mapboxgl.Marker()
             .setLngLat([parseFloat(lng), parseFloat(lat)])
             .setPopup(new mapboxgl.Popup().setHTML(`
-              <div class="bg-green-900 p-4">
-                <p class="text-lg font-medium text-green-200 opacity-80">${date}</p>
-                <h2 class="text-xl font-semibold text-green-50 tracking-tight leading-6">${name}</h2>
+              <div class="bg-green-900 p-4 relative">
+                <time datetime="${date.toISOString()}" title="${fullDate}" class="text-lg font-medium text-green-200 opacity-80">${dayOfWeek}</time>
+                <h2 class="text-xl font-black text-green-50 tracking-tight leading-6">${name}</h2>
               </div>
-              <div class="p-4">
-                <p><em>${address}</em></p>
-                <p>${description}</p>
-                ${website ? `<a href="${website}" target="_blank">Visit Website</a>` : ''}
+              <div class="p-4 gap-2 flex flex-col">
+                <p class="font-semibold leading-[14px]">${address}</p>
+                <p>${updatedDescription}</p>
+                ${url ? `<a href="${url}" target="_blank" class="top-3 right-3 absolute w-6 h-6 hover:text-green-500 focus:outline-none focus-visible:outline-1 focus-visible:outline-green-700 focus-visible:bg-green-800 rounded-full p-1">
+                  <svg data-slot="icon" fill="none" stroke-width="2" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25"></path>
+                  </svg>
+                </a>` : ''}
               </div>
+              ${image_tag}
             `))
             .addTo(map.current);
         });
