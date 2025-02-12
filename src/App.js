160aceb9df0bce5361e6c5a2c9e3fda3b270b85b
Stan <nolastan@gmail.com>
2025-02-12 07:19:33 +0000

    style popup

--- src/App.js
+++ src/App.js
@@ -32,15 +32,19 @@ export default function App() {
         shops.forEach(shop => {
           const { name, address, start, description, lat, lng, image, website } = shop;
           const image_tag = image ? `<img src="${image}" alt="" style="width: 100%; aspect-ratio: 2; object-fit: cover; border-radius: 3px;" />` : '';
-          const date = new Date(start).toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' })
+          const date = new Date(start).toLocaleDateString('en-US', { weekday: 'long' })
           new mapboxgl.Marker()
             .setLngLat([parseFloat(lng), parseFloat(lat)])
             .setPopup(new mapboxgl.Popup().setHTML(`
-              <h2 class="text-xl font-semibold text-green-50">${name}</h2>
-              <p class="text-lg font-medium text-green-100">${date}</p>
-              <p><em>${address}</em></p>
-              <p>${description}</p>
-              ${website ? `<a href="${website}" target="_blank">Visit Website</a>` : ''}
+              <div class="bg-green-900 p-4">
+                <p class="text-lg font-medium text-green-200 opacity-80">${date}</p>
+                <h2 class="text-xl font-semibold text-green-50 tracking-tight leading-6">${name}</h2>
+              </div>
+              <div class="p-4">
+                <p><em>${address}</em></p>
+                <p>${description}</p>
+                ${website ? `<a href="${website}" target="_blank">Visit Website</a>` : ''}
+              </div>
             `))
             .addTo(map.current);
         });
